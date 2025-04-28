83020f2cc2c5976d9b3da7d2c5f055f2cb67a92a
abdulrehman990 <mabdulf64@gmail.com>
2025-04-29 01:10:34 +0500

    Addition

---
+++ 33333333/script.js
@@ -0,0 +1,48 @@
+
+// var h =2;
+// var b= 2;
+// var c= "Abdul rehman";
+// var d= true;
+// var e= 23999.333333;
+// var f= null;
+
+// console.log(h,b,c,d,e,f)
+// console.log(typeof h,typeof b,typeof c,typeof d,typeof e, typeof f )
+
+// let a= {
+//     name: "Abdul rehman",
+//     class: "BSSE",
+//     subject: "chemistry,physics,mathematics",
+//     roll_no: 24399
+// }
+// console.log (a)
+// a.section = "A";
+// console.log(a)
+
+// var a= "Abdul rehman"
+// var b= 2
+
+// console.log(a , + b)
+// console.log(typeof "Abdul rehman")
+
+// const a= {
+//     name: "Abdul rehman",
+//     class: "BSSE",
+//     subject: "chemistry,physics,mathematics",
+//     roll_no: 24399
+// }
+// console.log (a)
+// a.section = "A";
+// console.log(a)
+
+// Word-Meaning Dictionary
+let dictionary = {
+    "Eloquent": "Fluent or persuasive in speaking or writing",
+    "Benevolent": "Well-meaning and kindly",
+    "Candid": "Truthful and straightforward; frank",
+    "Vivid": "Producing powerful feelings or strong, clear images in the mind",
+    "Diligent": "Showing care and effort in your work or duties"
+}
+
+// Display the dictionary
+console.log("Word Meaning Dictionary:" , dictionary)
